0b5c588c5610af82ea46d3f75d0a75ced8374e0a
mehar-srinivas-chowdari <84114918+mehar-srinivas-chowdari@users.noreply.github.com>
2022-03-28 21:22:22 +0530

    Create Equilibrium Point.cpp

---
+++ Equilibrium Point.cpp
@@ -0,0 +1,14 @@
+class solution{ 
+   public:
+    // Function to find equilibrium point in the array.
+    // a: input array
+    // n: size of array
+    int equilibriumPoint(long long a[], int n) {
+        // Your code here
+        if(n==1) return 1;
+        for(int i=1;i<n;i++) a[i]=a[i-1]+a[i];
+        for(int i=1;i<n;i++) if(a[i-1]==a[n-1]-a[i]) return i+1;
+        return -1;
+    }
+
+};
